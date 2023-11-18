b4f7c87b1f8371e94d4bdb2f3d95e24d585607f4
Tariq <tariqqureshey@gmail.com>
2023-11-18 09:15:36 -0500

    Test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//Test
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
